dde51e1556b8fb4eef07d9263e1244e6325ece62
zhangtao <zhangtao@changxinsec.com>
2023-10-24 22:30:28 +0800

    first commit

---
+++ hazel/src/Hazel/EntryPoint.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+
+#ifdef HZ_PLATFORM_WINDOWS
+extern Hazel::Application* Hazel::CreateApplication();
+
+int main(int argc, char** argv)
+{
+	std::cout << "Hello Engine" << std::endl;
+	auto app = Hazel::CreateApplication();
+
+	app->Run();
+
+	delete app;
+}
+
+#endif
