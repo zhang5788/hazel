8716a827740c21601804b5ad1f0c92908c0f8eaa
zhangtao <zhangtao@changxinsec.com>
2023-10-24 23:39:04 +0800

    add log system

--- hazel/src/Hazel.h
+++ hazel/src/Hazel.h
@@ -2,6 +2,7 @@
 
 // For use by  Hazel applications
 #include "Hazel/Application.h"
+#include "HAzel/Log.h"
 
 // ---Entry Point -----------------------------
 #include "Hazel/EntryPoint.h"
--- hazel/src/Hazel/EntryPoint.h
+++ hazel/src/Hazel/EntryPoint.h
@@ -6,11 +6,14 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv)
 {
-	std::cout << "Hello Engine" << std::endl;
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized Log!");
+
+	int a = 10;
+	HZ_CORE_INFO("Hello, Var={0}", a);
+
 	auto app = Hazel::CreateApplication();
-
 	app->Run();
-
 	delete app;
 }
 
